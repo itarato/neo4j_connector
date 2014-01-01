95522744226653792651d662f5c2616462ee686c
Peter Arato <it.arato@gmail.com>
2014-01-01 22:36:45 +0000

    Add menu tabs.

---
+++ lib/Drupal/neo4j_connector/Controller/Neo4JPageController.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itarato
+ * Date: 1/1/14
+ * Time: 9:41 PM
+ */
+
+namespace Drupal\neo4j_connector\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\neo4j_connector\Neo4JDrupal;
+
+class Neo4JPageController extends ControllerBase {
+
+  /**
+   * Page callback of the node graph tab page.
+   *
+   * @param $node
+   *  Node object
+   * @return string
+   */
+  public function nodeGraphInfo(EntityInterface $node) {
+    return $this->graphInfoPageContent('node', $node->id());
+  }
+
+  /**
+   * Page callback of the user graph tab page.
+   *
+   * @param $account
+   *  User object.
+   * @return string
+   */
+  public function userGraphInfo(EntityInterface $account) {
+    return $this->graphInfoPageContent('user', $account->id());
+  }
+
+  /**
+   * Page callback og the term graph tab page.
+   *
+   * @param $term
+   *  Term object.
+   * @return string
+   */
+  public function termGraphInfo(EntityInterface $term) {
+    return $this->graphInfoPageContent('taxonomy_term', $term->id());
+  }
+
+  /**
+   * Helper to generate a graph info about an entity.
+   *
+   * @param $entity_type
+   * @param $id
+   * @return null|string
+   */
+  protected function graphInfoPageContent($entity_type, $id) {
+    $graph_node = Neo4JDrupal::sharedInstance()->getGraphNodeOfEntity($entity_type, $id);
+
+    if (!$graph_node) {
+      return t('Cannot find associated graph node.');
+    }
+
+    $info = array();
+    $info[] = t('Graph node ID: <strong>@nodeid</strong>', array('@nodeid' => $graph_node->getId()));
+    $data_browser_link = 'http://localhost:7474/webadmin/#/data/search/' . $graph_node->getId() . '/';
+    $info[] = t('Data browser link') . ': ' . l($data_browser_link, $data_browser_link);
+    $info[] = '<pre>START n=node:' . NEO4J_CONNECTOR_ENTITY_INDEX_PREFIX . $entity_type . '(entity_id="' . $id . '") RETURN n;</pre>';
+
+    return theme('item_list', array('items' => $info));
+  }
+
+}
