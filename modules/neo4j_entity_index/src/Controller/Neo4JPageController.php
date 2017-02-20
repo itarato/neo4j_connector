6d9bae08e67b292a051cf7b7b9e4936c2b366be8
Peter Arato <it.arato@gmail.com>
2017-02-20 22:49:43 +0800

    Fix node page addition.

--- modules/neo4j_entity_index/src/Controller/Neo4JPageController.php
+++ modules/neo4j_entity_index/src/Controller/Neo4JPageController.php
@@ -11,6 +11,10 @@ namespace Drupal\neo4j_entity_index\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityInterface;
 
+/**
+ * Class Neo4JPageController
+ * @package Drupal\neo4j_entity_index\Controller
+ */
 class Neo4JPageController extends ControllerBase {
 
   /**
@@ -27,7 +31,7 @@ class Neo4JPageController extends ControllerBase {
   /**
    * Page callback of the user graph tab page.
    *
-   * @param $user
+   * @param EntityInterface $user
    *  User object.
    * @return string
    */
@@ -38,7 +42,7 @@ class Neo4JPageController extends ControllerBase {
   /**
    * Page callback og the term graph tab page.
    *
-   * @param $term
+   * @param EntityInterface $term
    *  Term object.
    * @return string
    */
@@ -49,8 +53,8 @@ class Neo4JPageController extends ControllerBase {
   /**
    * Helper to generate a graph info about an entity.
    *
-   * @param $entity_type
-   * @param $id
+   * @param string $entity_type
+   * @param string $id
    * @return null|string
    */
   protected function graphInfoPageContent($entity_type, $id) {
@@ -62,15 +66,12 @@ class Neo4JPageController extends ControllerBase {
 
     $info = array();
     $info[] = t('Graph node ID: <strong>@nodeid</strong>', array('@nodeid' => $graph_node->getId()));
-    $data_browser_link = 'http://localhost:7474/webadmin/#/data/search/' . $graph_node->getId() . '/';
-    $info[] = t('Data browser link') . ': ' . l($data_browser_link, $data_browser_link);
-    $info[] = '<pre>START n=node:' . NEO4J_CONNECTOR_ENTITY_INDEX_PREFIX . $entity_type . '(entity_id="' . $id . '") RETURN n;</pre>';
+    $info[] = ['#markup' => '<pre>MATCH (n:node) WHERE n.entity_id = "' . $id . '" RETURN n;</pre>'];
 
-    $info = array(
+    return array(
       '#theme' => 'item_list',
       '#items' => $info,
     );
-    return drupal_render($info);
   }
 
 }
