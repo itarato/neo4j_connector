e6fb9873a3e3e11c7024c303b843e12a267a66a4
Peter Arato <it.arato@gmail.com>
2017-03-06 23:23:38 +0800

    Remove old index code.

--- src/Form/Neo4JAdminForm.php
+++ src/Form/Neo4JAdminForm.php
@@ -35,13 +35,6 @@ class Neo4JAdminForm extends ConfigFormBase {
       '#description' => t('Port for the Neo4J server. Default is 7474.'),
     );
 
-    $form['index_immediately'] = array(
-      '#title' => t('Index immediately'),
-      '#type' => 'checkbox',
-      '#default_value' => $settings->get('index_immediately'),
-      '#description' => t('Set it if you would like to send items to the index immediately and do not want to wait for cron.'),
-    );
-
     return parent::buildForm($form, $form_state);
   }
 
@@ -49,7 +42,6 @@ class Neo4JAdminForm extends ConfigFormBase {
     $this->configFactory->getEditable('neo4j_connector.site')
       ->set('host', $form_state->getValue('host'))
       ->set('port', $form_state->getValue('port'))
-      ->set('index_immediately', $form_state->getValue('index_immediately'))
       ->save();
 
     parent::submitForm($form, $form_state);
