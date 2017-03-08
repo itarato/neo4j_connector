8b8a18bdd2cfc7a12501a6c9c25f756fd8f1e036
Peter Arato <it.arato@gmail.com>
2017-03-08 20:49:27 +0800

    Remove old index related items.

--- src/Controller/Neo4JController.php
+++ src/Controller/Neo4JController.php
@@ -8,13 +8,8 @@ namespace Drupal\neo4j_connector\Controller;
 
 use Drupal;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Url;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Neo4JController extends ControllerBase {
-
-  public function __construct() {
-  }
 
   public function adminSettings() {
     try {
@@ -28,47 +23,6 @@ class Neo4JController extends ControllerBase {
 
     $settings_form = Drupal::formBuilder()->getForm('Drupal\neo4j_connector\Form\Neo4JAdminForm');
     return $settings_form;
-  }
-
-  public function indexSettings() {
-    $index_info = neo4j_connector_index_info();
-
-    if (!$index_info) {
-      // Exit - no index.
-      return array(
-        '#markup' => t('No index enabled. Enable neo4j_entity_index or other index modules.'),
-      );
-    }
-
-    $rows = array();
-    foreach ($index_info as $key => $index) {
-      $mark_link = Drupal::l(t('Mark all for index'), new Url('neo4j_connector.index.purge', ['neo4j_connector_index' => $key]));
-      $setting_path = Drupal::l(t('settings'), new Url($index['settings path']));
-      $rows[] = array(
-        $index['label'],
-        $mark_link,
-        isset($index['settings path']) ? $setting_path : '',
-      );
-    }
-
-    $purge_form = Drupal::formBuilder()->getForm('Drupal\neo4j_connector\Form\Neo4JPurgeDBForm');
-    $reindex_form = Drupal::formBuilder()->getForm('Drupal\neo4j_connector\Form\Neo4JIndexForm');
-
-    $indexes = array(
-      '#theme' => 'table',
-      '#rows' => $rows,
-      '#header' => array(t('Index'), NULL, t('Settings')),
-    );
-
-    return array(
-      $indexes,
-      $purge_form,
-      $reindex_form,
-    );
-  }
-
-  public static function create(ContainerInterface $container) {
-    return new static();
   }
 
 }
